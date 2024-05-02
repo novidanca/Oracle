b146e15206f8f4935344f46c8853921fb155dae0
Ned Emerson <46656171+novidanca@users.noreply.github.com>
2024-05-02 11:16:26 -0400

    Update player page

--- Oracle.App/Components/Pages/Players/PlayersPage.razor.cs
+++ Oracle.App/Components/Pages/Players/PlayersPage.razor.cs
@@ -1,5 +1,18 @@
-﻿namespace Oracle.App.Components.Pages.Players;
+﻿#region using
+
+using Microsoft.EntityFrameworkCore;
+using Oracle.Data.Models;
+
+#endregion
+
+namespace Oracle.App.Components.Pages.Players;
 
 public partial class PlayersPage : OracleBasePage
 {
+	private List<Player> Players { get; set; } = [];
+
+	protected override async Task Refresh()
+	{
+		Players = await Db.Players.Include(x => x.Characters).ToListAsync();
+	}
 }
