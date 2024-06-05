342c9627d85254cc36885a496b65e6c7e34e1cae
Ned Emerson <46656171+novidanca@users.noreply.github.com>
2024-06-05 01:11:02 -0400

    Add some new components and classes to make working with the Timeline easier and faster

---
+++ Oracle.App/Components/Shared/Timeline/Models/TimelineDayStyleSpec.cs
@@ -0,0 +1,31 @@
+﻿namespace Oracle.App.Components.Shared.Timeline.Models;
+
+public class TimelineDayStyleSpec(int dayWidthPixels, int dayHeightPixels, int marginPixels)
+{
+	public int DayWidthPixels { get; set; } = dayWidthPixels;
+	public int DayHeightPixels { get; set; } = dayHeightPixels;
+	public int MarginPixels { get; set; } = marginPixels;
+
+	public static int BorderPixels => 2;
+
+	public int InnerSpanEndWidthPixels => DayWidthPixels - MarginPixels - BorderPixels;
+	public int InnerSpanHeightPixels => DayHeightPixels - BorderPixels * 2;
+
+	public int GetTimelineDayPixelWidth(int numDays = 1)
+	{
+		var numMarginPixels = numDays <= 1 ? 0 : MarginPixels * (numDays - 1);
+		var numPixels = DayWidthPixels * numDays;
+		numPixels += numMarginPixels;
+		return numPixels;
+	}
+
+	public int GetSpanInnerPixelWidth(int numDays)
+	{
+		var spanPixels = GetTimelineDayPixelWidth(numDays);
+		return spanPixels - BorderPixels * 2;
+	}
+
+	public TimelineDayStyleSpec() : this(36, 36, 1)
+	{
+	}
+}
